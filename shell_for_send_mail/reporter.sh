0e00e6846350ddb4138cf7ab96ce9c540ce04851
Alexclownfish <63449830+alexclownfish@users.noreply.github.com>
2022-02-15 14:22:38 +0800

    Create reporter.sh

---
+++ shell_for_send_mail/reporter.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+#auuthor:alex
+#shell for creating grafana dashboard report
+filepath=/opt/grafana/report/
+date=$(date +%Y-%m-%d-%H:%M)
+
+# dashboard report name
+filename_yunwei_ziyuan=运维资源全览-centernode-${date}.pdf
+
+# download grafana dashboard report
+wget -q -O ${filepath}${filename_yunwei_ziyuan} "http://172.22.254.57:30868/api/v5/report/viOR-qvnkdasd?apitoken=eyJrIjoiNjRZQjJlT2pKM1h2QVZLbTZWQ0pzMjFJSzdjSVFQUkYiLCJuIjoiZ3JhZmFuYS1yZXBvcnQiLCJpZCI6MX0=&from=now-24h&to=now&var-origin_prometheus=&var-Node=All&var-NameSpace=All&var-Container=All&var-Pod=All"
+
+sleep 30s
+
+# send email
+mail -i \ 
+-a ${filepath}${filename_yunwei_ziyuan} \
+-s "Grafana监控日报"-`date +%Y-%m-%d-%H:%M` \
+-c "21178857@qq.com" ywz0207@163.com < /opt/grafana/logs/send_mail.txt 
